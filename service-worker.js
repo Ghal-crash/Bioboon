c23ad944ac4c69a9257e78b49a50ace60be7f2e7
Galih Aditya <ghalih404@gmail.com>
2024-10-08 01:51:52 +0700

    Upload all

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const cacheName = 'bioboon-v1';
+const staticAssets = [
+  './',
+  './gps-tracking.html',
+  './aboutPage.html',
+  './assets/bioboon-img.png',
+  './assets/personImg/',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async event => {
+  const req = event.request;
+  const url = new URL(req.url);
+  
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    return await cache.match(req);
+  }
+}
